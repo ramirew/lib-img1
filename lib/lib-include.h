69e0e62181df09434cf76c0b7877b307de5895e0
ismaelfari1996 <abel_fari@hotmail.com>
2023-09-14 11:36:57 -0500

    Have all functions includes

---
+++ lib/lib-include.h
@@ -0,0 +1,11 @@
+#include "./svm-implement/svm-implement.h"
+#include "./utility/utility.h"
+#include "./utility/metrics.h"
+#include "./gaussian_filter/gaussianFilter.h"
+#include "./stb-implement/StbImageImplementation.h"
+#include "./filter-process/filter-process.h"
+#include "./canny/Canny.h"
+#include "./sobel/sobel.h"
+
+
+#define LIB_IS_VERSION 2.0
