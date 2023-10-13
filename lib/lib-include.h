19cb5797a4c91ce12942a1b9f78728df379c91c0
ismaelfari1996 <abel_fari@hotmail.com>
2023-10-13 18:33:35 -0500

    update

--- lib/lib-include.h
+++ lib/lib-include.h
@@ -1,11 +1,11 @@
-#include "./svm-implement/svm-implement.h"
+#include "./classifier/svm-implement/svm-implement.h"
 #include "./utility/utility.h"
 #include "./utility/metrics.h"
-#include "./gaussian_filter/gaussianFilter.h"
+#include "./preprocessing/gaussian_filter/gaussianFilter.h"
 #include "./stb-implement/StbImageImplementation.h"
 #include "./filter-process/filter-process.h"
-#include "./canny/Canny.h"
-#include "./sobel/sobel.h"
+#include "./edge_detector/canny/Canny.h"
+#include "./edge_detector/sobel/sobel.h"
 
 
 #define LIB_IS_VERSION 2.0
