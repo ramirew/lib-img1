b343891b37581101913cbd24991e64c9e45410cf
ismaelfari1996 <abel_fari@hotmail.com>
2023-10-13 20:40:43 -0500

    Lib includes

--- lib/lib-include.h
+++ lib/lib-include.h
@@ -1,9 +1,9 @@
 #include "./classifier/svm-implement/svm-implement.h"
 #include "./utility/utility.h"
+#include "./preprocessing/filter-process/filter-process.h"
 #include "./utility/metrics.h"
 #include "./preprocessing/gaussian_filter/gaussianFilter.h"
 #include "./stb-implement/StbImageImplementation.h"
-#include "./preprocessing/filter-process/filter-process.h"
 #include "./edge_detector/canny/Canny.h"
 #include "./edge_detector/sobel/sobel.h"
 
