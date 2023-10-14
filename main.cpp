a404115126413dad661810947c286c7831729abf
ismaelfari1996 <abel_fari@hotmail.com>
2023-10-13 20:40:53 -0500

    update

--- main.cpp
+++ main.cpp
@@ -11,8 +11,14 @@ int main(){
     //Example to call any class
     StbImageImplementation stb;
     Utility u;
+    GaussianFilter g;
+    Canny c;
+    Sobel s;
     int width, height;
-    double** image=stb.imread("image.jpg",width,height);
-    u.free_memory(image,height);
+    double** image=stb.imread("/home/ismael/Documents/cara.jpg",width,height);
+    double thresh[2]={0.2,0.5};
+    double **sobel=s.applySobel(image,width,height);
+    stb.imwrite("img.jpeg",sobel,width,height);
+    u.free_memory(sobel,height);
     return 0;
 }
