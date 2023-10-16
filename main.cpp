0b45cab282f5cca6938de71b4ed3151021df5618
ismaelfari1996 <abel_fari@hotmail.com>
2023-10-16 15:03:29 -0500

    main

--- main.cpp
+++ main.cpp
@@ -14,12 +14,10 @@ int main(){
     GaussianFilter g;
     Canny c;
     Sobel s;
+    HistogramHandler h;
     int width, height;
-    double** image=stb.imread("/home/ismael/Documents/cara.jpg",width,height);
-    double thresh[2]={0.2,0.5};
-    int **sobel=s.applySobel(image,width,height);
-    stb.imwrite("img.jpeg",sobel,width,height);
-
-    u.free_memory(sobel,height);
+    double** image=stb.imread("image.jpg",width,height);
+    stb.imwrite("img.jpeg",image,width,height);
+    u.free_memory(image,height);
     return 0;
 }
