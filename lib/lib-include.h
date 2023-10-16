b739c4b6235887648585bf632e0b368f98b4235d
ismaelfari1996 <abel_fari@hotmail.com>
2023-10-16 15:02:58 -0500

    lib include

--- lib/lib-include.h
+++ lib/lib-include.h
@@ -1,6 +1,7 @@
 #include "./classifier/svm-implement/svm-implement.h"
 #include "./utility/utility.h"
 #include "./preprocessing/filter-process/filter-process.h"
+#include "./preprocessing/histogram/histogram.h"
 #include "./utility/metrics.h"
 #include "./preprocessing/gaussian_filter/gaussianFilter.h"
 #include "./stb-implement/StbImageImplementation.h"
